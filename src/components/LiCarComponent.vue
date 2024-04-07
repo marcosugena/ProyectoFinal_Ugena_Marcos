b81609705f988bde97a135cb577cf14e025efa82
Marcos Ugena <marcosugenaochando@gmail.com>
2024-04-07 22:59:36 +0200

    function carrito+ fixes and functionalities of the landing

---
+++ src/components/LiCarComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="containerli d-flex align-content-center justify-content-between align-items-center ">
+            <img :src="imageurl" alt="">
+            <li class="text-center w-25">{{name}}</li>
+            <li>X{{Cantidad}}</li>
+            <li>{{price}}€</li>
+            <li><img src="/papelera.png" alt="" id="papelera"></li>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            price:String,
+            imageurl:String,
+            name:String,
+            Cantidad:{
+            default:1,    
+            Number
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.containerli{
+    margin: 10px;
+    img{
+        width: 80px;
+    }
+    #papelera{
+        width: 32px;
+        margin-left: 20px;
+    }
+}
+   
+</style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,8 @@ const store = createStore({
   state: {
     usuarioLogueado: false,
     NombreUsuario:"",
-    IdUsuario:-1
+    IdUsuario:-1,
+    Carrito:[]
   },
   mutations: {
     setUsuarioLogueado(state, valor) {
@@ -15,7 +16,20 @@ const store = createStore({
     },
     setId(state, valor){
       state.IdUsuario = valor;
-    }
+    },
+    agregarAlCarrito(state, producto) {
+      const index = state.Carrito.findIndex(item => item.Id === producto.Id);
+  
+      if (index !== -1) {
+          // Si el producto ya está en el carrito, incrementa la cantidad
+          state.Carrito[index].Cantidad++;
+      } else {
+          // Si el producto no está en el carrito, agrégalo con cantidad 1
+          producto.Cantidad = 1;
+          state.Carrito.push(producto);
+      }
+  }
+  
   },
   actions: {
     login({ commit }) {
@@ -33,12 +47,16 @@ const store = createStore({
     IdUsuario({ commit }, id) {
       // Lógica de actualización del nombre de usuario
       commit('setId', id);
+    },
+    agregarAlCarrito({ commit }, producto) {
+      commit('agregarAlCarrito', producto);
     }
   },
   getters: {
     estadoUsuario: (state) => (state.usuarioLogueado ? 'Logueado' : 'No Logueado'),
     nombreDeUsuario: (state) => state.NombreUsuario,
-    Idusu: (state) => state.IdUsuario
+    Idusu: (state) => state.IdUsuario,
+    carrito: state => state.Carrito
   },
 });
 
