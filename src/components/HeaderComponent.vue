8fe121704198d0fd544e35082c0b0b2e4c316c9c
Marcos <marcosugenaochando@gmail.com>
2024-04-24 16:38:25 +0200

    a

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -118,6 +118,7 @@ export default {
     ocultarcarrito(){
       const div=document.getElementById("dropdownmenu2help");
       div.style.display="none";
+      
     },
     mostrarcarrito(){
       const div=document.getElementById("dropdownmenu2help");
